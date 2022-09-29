c5284539cfa1744594a876b469af1c3da3268bee
Ritwik Srivastava <Ritwik.Srivastava@Geminisolutions.com>
2022-09-29 17:38:36 +0530

    Added API Documentation

--- src/main/java/com/example/employee/controller/DepartmentController.java
+++ src/main/java/com/example/employee/controller/DepartmentController.java
@@ -2,6 +2,11 @@ package com.example.employee.controller;
 
 import com.example.employee.dto.DepartmentDTO;
 import com.example.employee.services.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +30,14 @@ public class DepartmentController {
      * @return List<Departments>
      * @throws Exception Throws exception when list is empty
      */
+    @Operation(summary = "Get all departments", description = "Get all the departments", tags = "Departments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found Departments",
+                    content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "No Department Found",
+                    content = @Content)
+    })
     @GetMapping("/departments")
     public ResponseEntity<Object> getDepartments() throws Exception {
         List<DepartmentDTO> departments = departmentService.getDepartments();
@@ -40,6 +53,14 @@ public class DepartmentController {
      * @return Department
      * @throws Exception Throws exception when department does not exist
      */
+    @Operation(summary = "Get department by Id", description = "Fetch Department using department Id", tags = "Departments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the department",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Department not found",
+                    content = @Content)
+    })
     @GetMapping("/departments/{deptId}")
     public ResponseEntity<Object> getDepartment(@PathVariable String deptId) throws Exception {
         Long id = Long.parseLong(deptId);
@@ -56,6 +77,13 @@ public class DepartmentController {
      * @return Success Message
      * @throws Exception when bad request
      */
+    @Operation(summary = "Add Department", description = "Add a departments", tags = "Departments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Department Added",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content)
+    })
     @PostMapping("/departments")
     public ResponseEntity<Object> createDepartment(@Valid @RequestBody DepartmentDTO department) throws Exception{
         departmentService.createDepartment(department);
@@ -72,6 +100,13 @@ public class DepartmentController {
      * @return Success Message
      * @throws Exception when bad request
      */
+    @Operation(summary = "Update Department", description = "Update the Department", tags = "Departments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Department Updated",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Department to be updated not found",
+                    content = @Content)
+    })
     @PutMapping("/departments/{deptId}")
     public ResponseEntity<Object> updateDepartment(@PathVariable String deptId, @Valid @RequestBody DepartmentDTO department) throws Exception{
         Long id = Long.parseLong(deptId);
@@ -88,6 +123,14 @@ public class DepartmentController {
      * @return Department
      * @throws Exception when department to be deleted does not exist
      */
+    @Operation(summary = "Delete Department", description = "Delete the departments", tags = "Departments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Department Deleted",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Department to be deleted not found",
+                    content = @Content)
+    })
     @DeleteMapping("/departments/{deptId}")
     public ResponseEntity<Object> deleteDepartment(@PathVariable String deptId) throws Exception{
         DepartmentDTO dept = departmentService.deleteDepartment(Long.parseLong(deptId));
--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.example.employee.controller;
 
+import com.example.employee.dto.DepartmentDTO;
 import com.example.employee.dto.EmployeeDTO;
 import com.example.employee.services.DepartmentService;
 import com.example.employee.services.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +34,14 @@ public class EmployeeController {
      * @return List<Employee>
      * @throws Exception when there are no employees
      */
+    @Operation(summary = "Get all Employees", description = "Get all the employees", tags = "Employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employees Found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "No Employee Found",
+                    content = @Content)
+    })
     @GetMapping("/employees")
     public ResponseEntity<Object> getEmployees() throws Exception{
         List<EmployeeDTO> employees = employeeService.getEmployees();
@@ -43,6 +57,14 @@ public class EmployeeController {
      * @return Employee
      * @throws Exception when employee does not exist
      */
+    @Operation(summary = "Get employee by Id", description = "Fetch Employee using employee Id", tags = "Employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content)
+    })
     @GetMapping("/employees/{employeeId}")
     public ResponseEntity<Object> getEmployee(@PathVariable String employeeId) throws Exception{
         Long id = Long.parseLong(employeeId);
@@ -60,6 +82,13 @@ public class EmployeeController {
      * @return Success Message
      * @throws Exception when bad request
      */
+    @Operation(summary = "Add Employee", description = "Add an employee", tags = "Employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Employee Added",
+                    content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content)
+    })
     @PostMapping("/departments/{deptId}/employees")
     public ResponseEntity<Object> createEmployee(@PathVariable String deptId, @Valid @RequestBody EmployeeDTO employeeDTO) throws Exception {
         Long id = Long.parseLong(deptId);
@@ -78,6 +107,13 @@ public class EmployeeController {
      * @return Success Message
      * @throws Exception when bad request
      */
+    @Operation(summary = "Update Employee", description = "Update the Employee", tags = "Employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee Updated",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Employee to be updated not found",
+                    content = @Content)
+    })
     @PutMapping("/departments/{deptId}/employees/{employeeId}")
     public ResponseEntity<Object> updateEmployee(@PathVariable String deptId, @PathVariable String employeeId, @Valid @RequestBody EmployeeDTO employeeDTO) throws Exception{
         employeeService.updateEmployee(Long.parseLong(deptId), Long.parseLong(employeeId), employeeDTO);
@@ -93,6 +129,14 @@ public class EmployeeController {
      * @return Employee
      * @throws Exception when employee to be deleted does not exist
      */
+    @Operation(summary = "Delete Employee", description = "Delete the employee", tags = "Employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee Deleted",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = DepartmentDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Employee to be deleted not found",
+                    content = @Content)
+    })
     @DeleteMapping("/employees/{employeeId}")
     public ResponseEntity<Object> deleteEmployee(@PathVariable String employeeId) throws Exception{
         EmployeeDTO employee = employeeService.deleteEmployee(Long.parseLong(employeeId));
